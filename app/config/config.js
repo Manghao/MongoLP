601ef8a86af1493de9054dbd1c387f2e75171431
Manghao <Manghao@users.noreply.github.com>
2018-01-05 03:27:11 +0100

    Delete config.js

--- app/config/config.js
+++
@@ -1,7 +0,0 @@
-exports.getConfig = () => {
-    return 'manghao-site_lpcissie:lpcissie@mongodb-manghao-site.alwaysdata.net/manghao-site_mongolp';
-};
-
-exports.getApiKey = () => {
-	return 'dac5bb197ce34ea4bfa005f5015666ef';
-}
